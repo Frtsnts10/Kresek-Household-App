dc0193a899fbdb0d8025daf8d84495bd97d72958
Filbert <filbert10.work@gmail.com>
2024-12-22 22:50:59 +0700

    nextjsconfig.ts fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import withPWA from "next-pwa";
 
 const nextConfig = withPWA({
-  reactStrictMode: true, // Enable React strict mode
-  swcMinify: true, // Use the SWC compiler for minification
   pwa: {
-    dest: "public", // Directory to output PWA assets like service workers
-    register: true, // Automatically register the service worker
-    skipWaiting: true, // Skip waiting and activate the new service worker immediately
+    dest: "public", // Output directory for service worker
+    register: true, // Auto-register the service worker
+    skipWaiting: true, // Activate updated service worker immediately
   },
+  reactStrictMode: true, // Ensure strict mode is enabled for React
+  swcMinify: true, // Enable SWC-based minification for faster builds
 });
 
 export default nextConfig;
